475dea52a121a62e504dd7579efbcfefc6484a50
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2022-03-31 21:40:31 +0700

    upload and create folder

---
+++ lib/views/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeState extends StatefulWidget {
+  const HomeState({ Key? key }) : super(key: key);
+
+  @override
+  State<HomeState> createState() => _HomeStateState();
+}
+
+class _HomeStateState extends State<HomeState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
